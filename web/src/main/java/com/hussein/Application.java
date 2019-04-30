4c8f9cea79162701ab4b15716c9134fcd496e1cd
hwangsy <siyuan.huang@fenbeitong.com>
2019-04-30 15:28:19 +0800

    biz-war

--- web/src/main/java/com/hussein/Application.java
+++ web/src/main/java/com/hussein/Application.java
@@ -2,6 +2,8 @@ package com.hussein;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 /**
  * <p>Title: Application</p>
@@ -11,9 +13,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @author hwangsy
  */
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(Application.class);
     }
 }
